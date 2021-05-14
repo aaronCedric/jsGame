3bd97d69ba47c4e6f952044c3f9a29bd3b199892
aaronCedric <aaronCedric.celino@gmail.com>
2021-05-13 23:51:10 -0500

    Added triggers and functions

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 'user strict';
 
+// Damage tracker for player and enemy
 let currentDmg = 0;
 let currentEnemyDmg = 0;
+
+// Turn count for trigger end
 let turnCounter = 1;
+
+// Still playing?
 let playing = true;
+
+// HP bars
+let playerHpWidth = 100;
+let enemyHpWidth = 100;
+
+// Triggers
+let phospTrig = true;
+let axionTrig = true;
+
 // hp bar for enemy and player
 const enemyHP = document.querySelector('.enemy--hp');
 const playerHP = document.querySelector('.player--hp');
 const enemyHpBar = document.querySelector('.hp--enemyBar');
 const playerHpBar = document.querySelector('.hp--playerBar');
+
 // btn variables
 const btnAttack = document.querySelector('.btn--attack');
 const btnSkl1 = document.querySelector('.btn--skill1');
 const btnSkl2 = document.querySelector('.btn--skill2');
 const btnSkl3 = document.querySelector('.btn--skill3');
 const btnSkl4 = document.querySelector('.btn--skill4');
+
 // log variables
 const attackLog = document.querySelector('.attack--log');
 const turnCount = document.querySelector('.turn--count');
@@ -23,6 +39,23 @@ const player = {
   maxHP: 1000,
   curHp: 1000,
   attack: 20,
+  inventory: {
+    twilCoat: {
+      name: 'Twil Coat',
+      desc: 'Mitigate 75% damage',
+      effVal: 75,
+    },
+    bandage: {
+      name: 'Bandage',
+      desc: 'Heals moderate amount of HP',
+      effVal: 200,
+    },
+    motDraft: {
+      name: 'Motivating Draft',
+      desc: 'Increase attack',
+      effval: 10,
+    },
+  },
   normalAtk: function () {
     currentDmg = this.attack + Math.round(Math.random() * 50);
   },
@@ -38,20 +71,29 @@ const player = {
 };
 
 const enemy = {
+  name: 'Lucilius',
   maxHp: 500,
   curHp: 500,
-  attack: 50,
-  paradiseLost: function () {
-    return (10000 / player.maxHP) * 50;
+  attack: function () {
+    return (currentEnemyDmg = Math.round(Math.random() * 20));
   },
-  phosporus: function () {},
-  axionApocalypse: function () {},
+  paradiseLost: function () {
+    return (currentEnemyDmg = (10000 / player.maxHP) * 50);
+  },
+  phosporus: function () {
+    return (currentEnemyDmg = (5000 / player.maxHP) * 20);
+  },
+  axionApocalypse: function () {
+    return (currentEnemyDmg = (7000 / player.maxHP) * 30);
+  },
   orbitalDarkness: function () {},
   iblis: function () {},
+  theEnd: function () {
+    return (currentEnemyDmg = 999999);
+  },
 };
 
 const updatePlayerHp = function (enemyDmg) {
-  let playerHpWidth = 100;
   player.curHp -= enemyDmg;
   playerHpWidth = (player.curHp * 100) / player.maxHP;
   if (player.curHp <= 0) playerHpWidth = 0;
@@ -60,7 +102,6 @@ const updatePlayerHp = function (enemyDmg) {
 };
 
 const updateEnemyHp = function () {
-  let enemyHpWidth = 100;
   // width = (enemy.hp*100)/(enemy.maxHP)
   // Damage to current hp
   enemy.curHp -= currentDmg;
@@ -75,7 +116,45 @@ const updateEnemyHp = function () {
 
 const battleStartTrigger = function () {
   updatePlayerHp(enemy.paradiseLost());
-  attackLog.innerText = `Enemy used Paradise Lost, dealing ${enemy.paradiseLost()} damage! \n`;
+  attackLog.innerText = `${
+    enemy.name
+  } used Paradise Lost, dealing ${enemy.paradiseLost()} damage! \n`;
+};
+
+const checkEnemyTrigger = function () {
+  if (enemyHpWidth <= 95 && enemyHpWidth >= 86 && phospTrig === true) {
+    updatePlayerHp(enemy.phosporus());
+    attackLog.innerText += `${
+      enemy.name
+    } used Phosporus, dealing ${enemy.phosporus()} damage! \n`;
+    phospTrig = false;
+    return true;
+  }
+  if (enemyHpWidth <= 85 && enemyHpWidth >= 75 && axionTrig === true) {
+    updatePlayerHp(enemy.axionApocalypse());
+    attackLog.innerText += `${
+      enemy.name
+    } used Axion Apocalypse, dealing ${enemy.axionApocalypse()} damage! \n`;
+    axionTrig = false;
+    return true;
+  }
+
+  if (turnCounter === 40) {
+    updatePlayerHp(enemy.theEnd());
+    attackLog.innerText += `${enemy.name} used The End, Game Over! \n`;
+    return true;
+  }
+};
+
+const enemyAttack = function () {
+  updatePlayerHp(enemy.attack());
+  attackLog.innerText += `${enemy.name} attack, dealing ${currentEnemyDmg} \n`;
+};
+
+const updateTurnCount = function () {
+  playing ? (turnCounter += 1) : pass;
+  checkEnemyTrigger() || enemyAttack();
+  turnCount.textContent = `Turn Count: ${turnCounter}`;
 };
 
 const attackFormula = function (playerMove) {
@@ -98,30 +177,39 @@ const attackFormula = function (playerMove) {
       attackLog.innerText += `Player wins!`;
       enemyHP.textContent = 0;
     }
+    if (player.curHp <= 0) {
+      playing = false;
+      attackLog.innerText += `You lose!`;
+      playerHP.textContent = 0;
+    }
   }
 };
 
 battleStartTrigger();
 
 btnSkl1.addEventListener('click', function () {
+  updateTurnCount();
   attackFormula(player.fireBolt);
 });
 
 btnSkl2.addEventListener('click', function () {
+  updateTurnCount();
   attackFormula(player.swipe);
 });
 
 btnSkl3.addEventListener('click', function () {});
 
 btnAttack.addEventListener('click', function () {
-  turnCounter += 1;
-  turnCount.textContent = `Turn Count: ${turnCounter}`;
   attackFormula(player.normalAtk);
+  updateTurnCount();
 });
 
-// To do
+// To do //
 // Add charge diamond for enemy
 // Make a cooldown for skills
-// Add trigger Hp
+// Add trigger Hp [sligh done]
 // Fix skills
-// Add more skills for enemy
+// Add more skills for enemy [slight done]
+// Auto scroll log
+// Make new sprite
+// Modal for execute skill
